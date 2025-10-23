90e8b9cb10c2e601bd1117da40b2635a8a88770f
Hasratdeep <hasratdeepkaur383@gmail.com>
2025-10-23 18:15:03 -0500

    Added morgan and dotenv for environment management.

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,14 @@ import app from "./app";
 import { Server } from "http";
 
 const PORT: string | number = process.env.PORT || 3000;
+const NODE_ENV: string = process.env.NODE_ENV || "development";
+const LOG_LEVEL: string = process.env.LOG_LEVEL || "dev";
 
 const server: Server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
+    console.log(`Log Level: ${LOG_LEVEL}`);
+    
 });
 
 export default server;
